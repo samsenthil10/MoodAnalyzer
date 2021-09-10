b317f6b6f9cc86f3ab7ee6ff2d4f56fe4fa4743e
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-11 04:37:11 +0530

    Added: Test case to validate happy mood for any mood messages

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -5,11 +5,18 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
-	
+
 	@Test
 	public void givenMessage_WhenSad_ShouldReturnSad() {
 
 		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-		assertEquals("SAD", moodAnalyzer.analyzeMood("This is a sad message") );
+		assertEquals("SAD", moodAnalyzer.analyzeMood("This is a sad message"));
+	}
+
+	@Test
+	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		assertEquals("HAPPY", moodAnalyzer.analyzeMood("This is a happy message"));
 	}
 }
