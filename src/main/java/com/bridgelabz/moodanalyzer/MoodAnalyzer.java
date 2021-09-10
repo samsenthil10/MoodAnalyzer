87e95e582bfcc0f71d674fe9595df8b97d823e71
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-11 04:34:22 +0530

    Added: To initialize project structure

---
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.moodanalyzer;
+
+public class MoodAnalyzer {
+	
+	public String analyzeMood(String message) {
+
+		if(message.toLowerCase().contains("sad"))
+			return "SAD";
+		return "HAPPY";
+	}
+}
