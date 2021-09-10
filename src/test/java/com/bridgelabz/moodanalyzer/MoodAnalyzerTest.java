2292b8aaa43a83c417c38c622a16ebbba356f902
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-11 05:16:51 +0530

    Deleted: Previous test cases to adopt use case 3

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -5,11 +5,6 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class MoodAnalyzerTest {
-
-	@Test
-	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
-
-		MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-		assertEquals("HAPPY", moodAnalyzer.analyzeMood());
-	}
+	
+	
 }
