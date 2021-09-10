392cd4467d3f7dc19b8c15cdfa7cf26b0efd4ed0
Senthil Kumar S A <sasenthilk@gmail.com>
2021-09-11 04:36:30 +0530

    Added: Test case to validate sad message

--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyzerTest.java
@@ -1,6 +1,15 @@
 package com.bridgelabz.moodanalyzer;
 
-public class MoodAnalyzerTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
 	
+	@Test
+	public void givenMessage_WhenSad_ShouldReturnSad() {
+
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		assertEquals("SAD", moodAnalyzer.analyzeMood("This is a sad message") );
+	}
 }
